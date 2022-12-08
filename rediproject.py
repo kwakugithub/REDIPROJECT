64aba13ffd721000a4b36ae0b6667d9bb00539ae
Kwaku Opare <kwakuoparefavor@gmail.com>
2022-12-08 00:59:13 +0000

    First commit

---
+++ rediproject.py
@@ -0,0 +1,14 @@
+import random
+
+top_of_range = input("Type a number: ")
+
+if top_of_range.isdigit():
+    top_of_range = int(top_of_range)
+
+    if top_of_range <= 0:
+        print("Please type a number larger than 0 next time.")
+        quit()
+
+else:
+    print("Please type a number nex time.")
+    quit()
